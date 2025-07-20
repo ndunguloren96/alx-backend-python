b4bde0a0b71e08acc54c9ba1b100cd55a47e34d9
Loren Ndung'u <ndunguloren96@gmail.com>
2025-07-20 12:00:26 +0300

    Configure URLs for conversations and messages

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = router.urls
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -2,7 +2,7 @@
 URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from chats.urls import router as chats_router # Import your router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(chats_router.urls)), # Include your API routes
 ]
